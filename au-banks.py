9cc006950b152e23e61c658653530061e0ca4b16
Francois Marier <francois@debian.org>
2013-08-11 23:18:41 +1200

    Initial version of the scripts

---
+++ au-banks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import requests
+
+sites = [
+  ('ANZ', 'https://www.anz.com/INETBANK/bankmain.asp'),
+  ('Bank of China', 'https://ebs.boc.cn/BocnetClient/LoginFrameAbroad.do?_locale=en_US'),
+  ('Bank of Melbourne', 'https://ibanking.bankofmelbourne.com.au/ibank/loginPage.action'),
+  ('Bankwest', 'https://ibs.bankwest.com.au/BWLogin/rib.aspx'),
+  ('Bendigobank', 'https://www.bendigobank.com.au/banking/BBLIBanking/'),
+  ('Bank of Queensland', 'https://www.ib.boq.com.au/boqbl'),
+  ('Citibank', 'https://www.citibank.com.au/AUGCB/JSO/signon/DisplayUsernameSignon.do'),
+  ('Commonwealth Bank', 'https://www.my.commbank.com.au/netbank/Logon/Logon.aspx'),
+  ('Heritage Bank', 'https://online.hbs.net.au/hbsv47/ntv471.asp?wci=entry'),
+#  ('HSBC' , 'https://www.hsbc.com.au/1/2/HUB_IDV2/IDV_EPP?__IWCountry=US&__IWLang=en&__Destination=HUB_IDV_CUSTOMER_MIGRATION&__menuType=__REGISTRATION&__registrationType=PIB-Registration'),
+  ('Mebank', 'https://ib.mebank.com.au/ME'),
+  ('NAB', 'https://ib.nab.com.au/nabib/index.jsp'),
+  ('Rabobank', 'https://secure.rabodirect.com.au/exp/policyenforcer/pages/loginB2CDGPEN.jsf?login'),
+  ('St. George', 'https://ibanking.stgeorge.com.au/ibank/loginPage.action'),
+  ('Suncorp Bank', 'https://internetbanking.suncorpbank.com.au/'),
+  ('Westpac', 'https://online.westpac.com.au/esis/Login/SrvPage'),
+]
+
+for site in sites:
+  r = requests.get(site[1], verify=False,  # Disabling cert checking, do not do this at home!
+                   headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'})
+  if 'strict-transport-security' in r.headers:
+    print site[0] + ': yes! [' + r.headers['strict-transport-security'] + ']'
+  else:
+    print site[0] + ': no :('
---
+++ nz-banks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import requests
+
+sites = [
+  ('ASB', 'https://fnc.asbbank.co.nz/1/User/LogOn'),
+  ('ANZ', 'https://secure.anz.co.nz/IBCS/pgLogin'),
+#  ('BNZ', 'https://www.bnz.co.nz/ib/app/login'),
+#  ('HSBC', 'https://www.hsbc.co.nz/1/2/HUB_IDV2/IDV_EPP?__IWCountry=&__IWLang=en&__Destination=HUB_IDV_CUSTOMER_MIGRATION&__menuType=__REGISTRATION&__registrationType=PIB-Registration'),
+  ('Kiwibank', 'https://www.ib.kiwibank.co.nz/'),
+  ('Rabobank', 'https://secure1.rabodirect.co.nz/exp/authenticationDGPEN.jsp'),
+  ('TSB', 'https://homebank.tsbbank.co.nz/online/'),
+  ('Westpac', 'https://sec.westpac.co.nz/IOLB/Login.jsp'),
+]
+
+for site in sites:
+  r = requests.get(site[1], verify=False,  # Disabling cert checking, do not do this at home!
+                   headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0'})
+  if 'strict-transport-security' in r.headers:
+    print site[0] + ': yes! [' + r.headers['strict-transport-security'] + ']'
+  else:
+    print site[0] + ': no :('
