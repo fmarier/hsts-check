7d64d70cc607065da73fed1d875a7ea7003ae571
Francois Marier <francois@mozilla.com>
2014-08-04 18:11:17 -0700

    Add ANZ Direct Online and SBS i-bank to the NZ list

--- nz-banks.py
+++ nz-banks.py
@@ -5,12 +5,14 @@ import requests
 sites = [
   ('ASB', 'https://fnc.asbbank.co.nz/1/User/LogOn'),
   ('ANZ', 'https://secure.anz.co.nz/IBCS/pgLogin'),
+  ('ANZ (Direct Online)', 'https://www.anzdirect.co.nz/online/EnterANZDirect.do'),
   ('BankDirect', 'https://vault.bankdirect.co.nz/default.asp'),
   ('BNZ', 'https://www.bnz.co.nz/ib/app/login'),
   ('HSBC', 'https://www.hsbc.co.nz/1/2/HUB_IDV2/IDV_EPP?__IWCountry=&__IWLang=en&__Destination=HUB_IDV_CUSTOMER_MIGRATION&__menuType=__REGISTRATION&__registrationType=PIB-Registration'),
   ('Kiwibank', 'https://www.ib.kiwibank.co.nz/'),
   ('Rabobank', 'https://secure1.rabodirect.co.nz/exp/authenticationDGPEN.jsp'),
   ('SBS', 'https://secure.sbsbank.co.nz/personal/'),
+  ('SBS (Community Banking)', 'https://ibank.sbs.net.nz/ui/inetbankindex.aspx'),
   ('TSB', 'https://homebank.tsbbank.co.nz/online/'),
   ('Westpac', 'https://sec.westpac.co.nz/IOLB/Login.jsp'),
 ]
